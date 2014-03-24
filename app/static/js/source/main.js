7fa8da220a21da8318ef97e00fe1da6deffdd50b
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-03-24 13:33:06 -0500

    metronome with samples

--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -1,123 +1,119 @@
 (function(){
 
   'use strict';
+
   var context;
   var drums;
-
+  var current16thNote;
+  var nextNoteTime = 0.0;
+  var lap = 25.0;
+  var lookAhead = 0.1;
+  var tempo = 120.0;
+  var isPlaying = false;
+  var noteLength = 0.05;
+  var timerID = 0;
+  var source;
 
   $(document).ready(init);
 
   function init(){
     $(document).foundation();
-    createContext();
-    //createSound();
+    createAudioContext();
     createDrums();
-    $('#kick').click(playKick);
-    $('#snare').click(playSnare);
-    $('#hat').click(playHat);
-    $('body').keydown(function(event){
-      if(event.keyCode === 65 || event.keyCode === 90){
-        playKick();
-        $('#kick').css('background-color', 'white');
-      }
-    });
-    $('body').keyup(function(event){
-      if(event.keyCode === 65 || event.keyCode === 90){
-        $('#kick').css('background-color', 'black');
-      }
-    });
-    $('body').keydown(function(e){
-      if(e.keyCode === 72){
-        playSnare();
-        $('#snare').css('background-color', 'white');
-      }
-    });
-    $('body').keyup(function(event){
-      if(event.keyCode === 72){
-        $('#snare').css('background-color', 'black');
-      }
-    });
-    $('body').keydown(function(e){
-      if(e.keyCode === 76){
-        playHat();
-        $('#hat').css('background-color', 'white');
-      }
-    });
-    $('body').keyup(function(event){
-      if(event.keyCode === 76){
-        $('#hat').css('background-color', 'black');
-      }
-    });
+    $('#play').click(play);
+    $('#setTempo').click(setTempo);
   }
 
-  function createContext(){
-    try{
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      context = new webkitAudioContext();
-    }catch(e){
-      alert('Web Audio API is not supported by this browser');
-    }
+///////////Setup////////////
+  function setTempo(){
+    tempo = $('#tempo').val()
   }
 
-  function createSound(){
-    var drums = new BufferLoader(context, ['../../audios/drums/kick.wav', '../../audios/drums/snare.wav', '../../audios/drums/hat.wav'], playBeat);
-    drums.load();
-  }
-
-  function playBeat(bufferList){
-    var kick = bufferList[0];
-    var snare = bufferList[1];
-    var hat = bufferList[2];
-    var startTime = context.currentTime + .100;
-    var tempo = 80;
-    var eighthNoteTime = (60/tempo)/2;
-
-    for(var bar = 0; bar < 2; bar++){
-      var time = startTime + bar * 8 * eighthNoteTime;
-
-      playSound(kick, time);
-      playSound(kick, time + 4 * eighthNoteTime);
-
-      playSound(snare, time + 2 * eighthNoteTime);
-      playSound(snare, time + 6 * eighthNoteTime);
-
-      for (var i = 0; i < 8; i++){
-        playSound(hat, time + i * eighthNoteTime);
-      }
-    }
-  }
-
-  function playSound(buffer, time){
-    var source = context.createBufferSource();
-    source.buffer = buffer;
-    source.connect(context.destination);
-    source.start(time);
+/////////Web Audio Drums///////////////
+  function createAudioContext(){
+    context = new webkitAudioContext();
   }
 
   function createDrums(){
-    drums = new BufferLoader(context, ['../../audios/drums/kick.wav', '../../audios/drums/snare.wav', '../../audios/drums/hat.wav'], playKick);
+    drums = new BufferLoader(context, ['../../audios/drums/kick.wav', '../../audios/drums/snare.wav', '../../audios/drums/hat.wav'], console.log('Welcome to MegaHurt'));
     drums.load();
   }
 
   function playKick(){
-    var source = context.createBufferSource();
+    source = context.createBufferSource();
     source.buffer = drums.bufferList[0];
     source.connect(context.destination);
     source.start(0);
   }
 
   function playSnare(){
-    var source = context.createBufferSource();
+    source = context.createBufferSource();
     source.buffer = drums.bufferList[1];
     source.connect(context.destination);
     source.start(0);
   }
 
   function playHat(){
-    var source = context.createBufferSource();
+    source = context.createBufferSource();
     source.buffer = drums.bufferList[2];
     source.connect(context.destination);
     source.start(0);
+  }
+
+//////////Play///////////////////////
+  function play(){
+    isPlaying = !isPlaying;
+
+    if(isPlaying){
+      current16thNote = 0;
+      nextNoteTime = context.currentTime;
+      scheduler();
+      return "stop";/////switch with toggle
+    }else{
+      window.clearTimeout(timerID);
+      return "play";//////switch with toggle
+    }
+  }
+
+  function scheduler(){
+    while(nextNoteTime < context.currentTime + lookAhead){
+      scheduleNote(current16thNote, nextNoteTime);
+      nextNote();
+    }
+    timerID = window.setTimeout(scheduler, lap);
+  }
+
+  function scheduleNote(beatNumber, time){
+   /* var osc = context.createOscillator();
+    osc.connect(context.destination);
+
+    if(beatNumber % 16 === 0){
+      osc.frequency.value = 220.0;
+    }else if(beatNumber % 4){
+      osc.frequency.value = 440.0;
+    }else{
+      osc.frequency.value = 880;
+    }
+
+    osc.start(time);
+    osc.stop(time + noteLength);*/
+
+    if(beatNumber % 16 === 0){
+      playKick();
+    }else if(beatNumber % 4){
+      playHat();
+    }else{
+      playSnare();
+    }
+  }
+
+  function nextNote(){
+    var secondsPerBeat = 60.0/tempo;
+    nextNoteTime += 0.25 * secondsPerBeat;
+    current16thNote++;
+    if(current16thNote === 16){
+      current16thNote === 0;
+    }
   }
 
 
