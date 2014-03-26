b0f786ae5e48d8a1b37cc17cbca6c5f2572be073
Samuel Tucker <samueljeffersontucker@gmail.com>
2014-03-26 15:35:16 -0500

    tom1

--- app/static/js/source/kit.js
+++ app/static/js/source/kit.js
@@ -14,10 +14,12 @@ function Kit(context, name, kNum) {
   var kickPath = this.pathName + 'kick.wav';
   var snarePath = this.pathName + 'snare.wav';
   var hatPath = this.pathName + 'hat.wav';
+  var tom1Path = this.pathName + 'tom1.wav';
 
   this.kickBuffer = 0;
   this.snareBuffer = 0;
   this.hatBuffer = 0;
+  this.tom1Buffer = 0;
 
   this.instrumentCount = kNum;
   this.instrumentLoadCount = 0;
@@ -25,8 +27,8 @@ function Kit(context, name, kNum) {
   this.loadSample(0, kickPath, false);
   this.loadSample(1, snarePath, false);
   this.loadSample(2, hatPath, false);
+  this.loadSample(3, tom1Path, false);
 
-  console.log(this);
 }
 //------ Loads Drum Samples ------//
 Kit.prototype.loadSample = function(sampleID, url, mixToMono) {
@@ -41,7 +43,7 @@ Kit.prototype.loadSample = function(sampleID, url, mixToMono) {
   request.onload = function(){
     self.context.decodeAudioData(
       request.response, function(buffer){
-        // var buffer = context.createBuffer(request.response, mixToMono);
+      //  var buffer = context.createBuffer(request.response, mixToMono);
         switch (sampleID) {
           case 0:
             self.kickBuffer = buffer;
@@ -52,6 +54,8 @@ Kit.prototype.loadSample = function(sampleID, url, mixToMono) {
           case 2:
             self.hatBuffer = buffer;
             break;
+          case 3:
+            self.tom1Buffer = buffer;
         }
         successCount++;
         self.instrumentLoadCount++;
--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -15,10 +15,83 @@
     $('.kick').click(clickKick);
     $('.snare').click(clickSnare);
     $('.hat').click(clickHat);
+    $('.tom1').click(clickTom1);
     $('.seqStep').click(clickSeqStep);
     $('select#kit').change(changeKit);
+    $('#volume').change(changeVolume);
+    $('#kick').click(playKick);
+    $('#snare').click(playSnare);
+    $('#hat').click(playHat);
+    $('#tom1').click(playTom1);
+    $('body').keydown(function(event){
+      if(event.keyCode === 65 || event.keyCode === 90){
+        playKick();
+        $('#kick').css('background-color', 'white');
+        $('#kick').css('color', 'black');
+      }
+    });
+    $('body').keyup(function(event){
+      if(event.keyCode === 65 || event.keyCode === 90){
+        $('#kick').css('background-color', 'black');
+        $('#kick').css('color', 'white');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode === 72){
+        playSnare();
+        $('#snare').css('background-color', 'white');
+        $('#snare').css('color', 'black');
+      }
+    });
+    $('body').keyup(function(event){
+      if(event.keyCode === 72){
+        $('#snare').css('background-color', 'black');
+        $('#snare').css('color', 'white');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode === 76){
+        playHat();
+        $('#hat').css('background-color', 'white');
+        $('#hat').css('color', 'black');
+      }
+    });
+    $('body').keyup(function(event){
+      if(event.keyCode === 76){
+        $('#hat').css('background-color', 'black');
+        $('#hat').css('color', 'white');
+      }
+    });
+    $('body').keydown(function(e){
+      if(e.keyCode === 82 || e.keyCode === 84){
+        playTom1();
+        $('#tom1').css('background-color', 'white');
+        $('#tom1').css('color', 'black');
+      }
+    });
+    $('body').keyup(function(event){
+      if(event.keyCode === 82 || event.keyCode === 84){
+        $('#tom1').css('background-color', 'black');
+        $('#tom1').css('color', 'white');
+      }
+    });
   }
 
+  function playKick(){
+    playNote(currentKit.kickBuffer);
+  }
+
+  function playSnare(){
+    playNote(currentKit.snareBuffer);
+  }
+
+  function playHat(){
+    playNote(currentKit.hatBuffer);
+  }
+
+  function playTom1(){
+    playNote(currentKit.tom1Buffer);
+  }
 //////// KITS ////////
   var kits = [];
   var kNumInstruments = 3; // called in drawDrumGrid(), drawPlayhead()
@@ -27,19 +100,20 @@
     'T09',
     'TR606',
   ];
-  //------ Initialize Drum Kits ------//
+
+ //------ Initialize Drum Kits ------//
   function initDrums(){
     var numKits = kitNames.length;
     for (var i = 0; i < numKits; i++) {
       kits.push(new Kit(context, kitNames[i], kNumInstruments));
     }
     changeKit();
-    console.log(kits);
   }
 
 //////// CONTROLS ////////
   var currentKit;
   var tempo = 120.0;
+  var gainVolume = 1.0;
 
   function setTempo(){
     tempo = $('#tempo').val();
@@ -57,7 +131,16 @@
       currentKit = kits[2];
     }
   }
+
+  function changeVolume(){
+    var volume = $('#volume').attr('data-slider');
+    gainVolume = volume/50;
+  }
 //////////Sequencer//////////////
+  var kickQueue=[];
+  var snareQueue=[];
+  var hatQueue=[];
+  var tom1Queue=[];
 
   function clickKick(){
     if(!$(this).hasClass('selected')){
@@ -83,6 +166,14 @@
     }
   }
 
+  function clickTom1(){
+    if(!$(this).hasClass('selected')){
+      tom1Queue.push(parseInt($(this).attr('tom1-sequence-position')));
+    }else{
+      tom1Queue = _.without(tom1Queue, parseInt($(this).attr('tom1-sequence-position')));
+    }
+  }
+
   function clickSeqStep(){
     if(!$(this).hasClass('selected')){
       $(this).addClass('selected');
@@ -93,24 +184,10 @@
 
 /////////Web Audio Drums///////////////
   var context;
-  var source;
 
   function createAudioContext(){
     context = new webkitAudioContext();
   }
-
-  /*function createDrums(){
-    drums = new BufferLoader(context,
-                            ['../../audios/Plasticid-MkII/kick.wav',
-                             '../../audios/Plasticid-MkII/snare.wav',
-                             '../../audios/Plasticid-MkII/hat.wav'],
-                             dummyFunction);
-    drums.load();
-  }
-
-  function dummyFunction(){
-    console.log('maybe');
-  }*/
 
 
 //////////Play///////////////////////
@@ -119,11 +196,9 @@
   var nextNoteTime = 0.0;
   var lap = 10.0;
   var lookAhead = 0.1;
-  var kickQueue=[];
-  var snareQueue=[];
-  var hatQueue=[];
   var isPlaying = false;
   var timerID = 0;
+  var source;
   //var nyquist = sampleRate * 0.5;
 
 
@@ -150,36 +225,64 @@
     timerID = window.setTimeout(scheduler, lap);
   }
 
-  function scheduleNote(beatNumber, time){
+  function scheduleNote(beatNumber){
     if(_.contains(kickQueue, beatNumber)){
       if(_.contains(snareQueue, beatNumber)){
         if(_.contains(hatQueue, beatNumber)){
-          playNote(currentKit.kickBuffer);
-          playNote(currentKit.snareBuffer);
-          playNote(currentKit.hatBuffer);
-        }else{
+          if(_.contains(tom1Queue, beatNumber)){
+            playNote(currentKit.kickBuffer);
+            playNote(currentKit.snareBuffer);
+            playNote(currentKit.hatBuffer);
+            playNote(currentKit.tom1Buffer);
+          }
+          else{
+            playNote(currentKit.kickBuffer);
+            playNote(currentKit.snareBuffer);
+            playNote(currentKit.hatBuffer);
+          }
+        }
+        else{
           playNote(currentKit.kickBuffer);
           playNote(currentKit.snareBuffer);
         }
-      }else{
+      }
+      else{
         playNote(currentKit.kickBuffer);
       }
     }else if(_.contains(snareQueue, beatNumber)){
       if(_.contains(hatQueue, beatNumber)){
-        playNote(currentKit.snareBuffer);
-        playNote(currentKit.hatBuffer);
+        if(_.contains(tom1Queue, beatNumber)){
+          playNote(currentKit.snareBuffer);
+          playNote(currentKit.hatBuffer);
+          playNote(currentKit.tom1Buffer);
+        }else{
+          playNote(currentKit.snareBuffer);
+          playNote(currentKit.hatBuffer);
+        }
       }else{
         playNote(currentKit.snareBuffer);
       }
     }else if(_.contains(hatQueue, beatNumber)){
-      playNote(currentKit.hatBuffer);
+      if(_.contains(tom1Queue, beatNumber)){
+        playNote(currentKit.hatBuffer);
+        playNote(currentKit.tom1Buffer);
+      }
+      else{
+        playNote(currentKit.hatBuffer);
+      }
+    }else if(_.contains(tom1Queue, beatNumber)){
+      playNote(currentKit.tom1Buffer);
     }
+    
   }
 
   function playNote(buffer){
+    var gainNode = context.createGain();
+    gainNode.gain.value = gainVolume;
     source = context.createBufferSource();
     source.buffer = buffer;
-    source.connect(context.destination);
+    source.connect(gainNode);
+    gainNode.connect(context.destination);
     source.start(0);
   }
 
